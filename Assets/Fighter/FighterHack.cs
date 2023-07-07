c13085686e202f3254b04db5c175a63dd475d213
ageneric-win <55108448+ageneric@users.noreply.github.com>
2023-07-07 22:38:28 +0100

    Add Fighter spawner test

---
+++ Assets/Fighter/FighterHack.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FighterHack : MonoBehaviour
+{
+    private Fighter fighter;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fighter = gameObject.GetComponent<Fighter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fighter.hack == "defaultHack")
+        {
+            fighter.health += 0.1f;
+        }
+    }
+}
