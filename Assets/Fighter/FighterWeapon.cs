7353a4ca67bf407021b2dadc7884c623a49dd728
ageneric-win <55108448+ageneric@users.noreply.github.com>
2023-07-09 13:20:39 +0100

    Add weapon sprite resources

---
+++ Assets/Fighter/FighterWeapon.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterWeapon : MonoBehaviour
+{
+    public Sprite[] weaponSprites;
+    public enum Weapon {Pistol, Rifle, SawnOff, Shotgun, SMG, Sniper};
+
+    public Weapon weapon = Weapon.Pistol;
+    public SpriteRenderer heldWeaponSprite;
+
+    public void Start()
+    {
+        heldWeaponSprite.sprite = weaponSprites[weapon];
+    }
+
+    public void Shoot()
+    {
+
+    }
+}
+
+public class Rifle
+{
+
+}
