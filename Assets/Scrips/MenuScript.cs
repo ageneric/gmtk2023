81673e83cff4bf604a797f565db0fc7a40a47d00
maximvassiliev <vassiliev.max@gmail.com>
2023-07-09 01:16:57 +0100

    menu

--- Assets/Scrips/MenuScript.cs
+++ Assets/Scrips/MenuScript.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
     public GameObject menu;
     public GameObject tutorial;
+    public GameObject credits;
 
     private void Start()
     {
         menu.SetActive(true);
         tutorial.SetActive(false);
+        credits.SetActive(false);
     }
     public void showTutorial()
     {
@@ -22,5 +25,22 @@ public class MenuScript : MonoBehaviour
     {
         menu.SetActive(true);
         tutorial.SetActive(false);
+    }
+
+    public void showCredits()
+    {
+        menu.SetActive(false);
+        credits.SetActive(true);
+    }
+
+    public void hideCredits()
+    {
+        menu.SetActive(true);
+        credits.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
